1a7b0f728457198a0e1fe4941f19758a2e015c44
Flopiras <Flopiras@yahoo.it>
2023-07-19 16:44:04 +0200

    fix input max

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ function randomPassword($length, $characters)
         <form action="index.php">
             <div class="mb-3 d-flex ">
                 <label for="length" class="form-label">Quanto deve essere lunga la tua password?</label>
-                <input type="number" name="length" class="form-control ms-4" id="length" min="1" max="10" style="width: 100px;">
+                <input type="number" name="length" class="form-control ms-4" id="length" min="1" max="30" style="width: 100px;">
             </div>
 
             <button type="submit" class="btn btn-primary">Genera password</button>
