016f83ceed534c225b9143c410090a032e2f3b80
Flopiras <Flopiras@yahoo.it>
2023-07-19 17:31:49 +0200

    add bootstrap

--- password.php
+++ password.php
@@ -13,11 +13,12 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' integrity='sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==' crossorigin='anonymous' />
 </head>
 
 <body>
-    <div>
-        <h4>Complimenti!</h4>
+    <div class="container my-4">
+        <h4 class="text-success">Complimenti!</h4>
         <p>La tua password è <strong><?= $_SESSION['password'] ?> </strong></p>
     </div>
 </body>
